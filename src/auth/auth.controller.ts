30cc9b979e212a717973414a2f3fd2be04678c75
Claudio Araujo <77213711+Tlakuache123@users.noreply.github.com>
2025-01-21 22:26:51 -0600

    refactor: move public decorator to decoratos folder

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Get, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './local-auth.guard';
 import { AuthService } from './auth.service';
 import { JwtAuthGuard } from './jwt-auth.guard';
-import { Public } from 'src/helpers/public.decorator';
+import { Public } from 'src/decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
